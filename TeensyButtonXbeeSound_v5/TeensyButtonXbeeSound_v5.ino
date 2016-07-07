161fa8de9508d62ddfe79ee73f16e004d6849316
adellelin <adelle.lin@gmail.com>
2016-07-06 21:33:56 -0700

    2 channel audio player

--- TeensyButtonXbeeSound_v5/TeensyButtonXbeeSound_v5.ino
+++ TeensyButtonXbeeSound_v5/TeensyButtonXbeeSound_v5.ino
@@ -97,17 +97,24 @@ char* soundFileName[] =
 //
 // buttons
 //
-const int buttonDebounceMs = 50;
+const int buttonDebounceMs = 30;
 const int buttonCount = 3;
-int buttonPin[] = {3, 4, 5};
+int buttonPin[] = {2, 3, 4};
 Bounce button[buttonCount];
+
+//
+// leds
+//
+const int ledPin[] = {16, 17, 8};
+int ledState = LOW;
+unsigned long ledStartMillis[] = {0, 0, 0};
+unsigned long blinkInterval = 100;
 
 //
 // mushroom selection
 //
 int mushroomCount = 4;
-int mushroomSelectorPin[] = { 20, 21, A10, A11 };
-
+int mushroomSelectorPin[] = { 20, 21 };
 
 //
 // sound mapping - first "row" is mushroom 0
@@ -124,24 +131,31 @@ char soundCommand[] = {
 void setup() {
   // debug / kwyboard // usb
   setupAudio();
-  
+
   Serial.begin(9600);
 
   // xbee (pin 0 and 1)
   Serial1.begin(9600);
-  
+
+  NVIC_SET_PRIORITY(IRQ_UART0_STATUS, 255); // 255 = lowest priority
+  NVIC_ENABLE_IRQ(IRQ_UART0_STATUS);
+
+ NVIC_SET_PRIORITY(IRQ_UART1_STATUS, 255); // 255 = lowest priority
+  NVIC_ENABLE_IRQ(IRQ_UART1_STATUS);
+
+
   setupMushroomSelection();
   setupButtons();
-  
+  setupLeds();
   return;
 }
 
 void setupAudio() {
-  
-// said this is needed for simultaneous cd card access
+
+  // said this is needed for simultaneous cd card access
   //AudioNoInterrupts();
 
-   AudioMemory(8);
+  AudioMemory(8);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.5);
 
@@ -153,29 +167,38 @@ void setupAudio() {
       delay(500);
     }
   }
-  mixer1.gain(0, 0.1);
+  mixer1.gain(0, 0.3);
   mixer1.gain(1, 0.3);
   mixer1.gain(2, 0.3);
-  mixer1.gain(3, 0.1);
+  mixer1.gain(3, 0.3);
   mixer2.gain(0, 0.3);
   mixer2.gain(1, 0.3);
   mixer2.gain(2, 0.3);
-  mixer2.gain(3, 0.1);
+  mixer2.gain(3, 0.3);
   mixer3.gain(0, 0.3);
   mixer3.gain(1, 0.3);
   mixer3.gain(2, 0.3);
-  mixer3.gain(3, 0.1);
+  mixer3.gain(3, 0.3);
   mixer4.gain(0, 0.3);
   mixer4.gain(1, 0.3);
   mixer4.gain(2, 0.3);
-  mixer4.gain(3, 0.1);
-  mixer5.gain(0, 0.5);
+  mixer4.gain(3, 0.3);
+  mixer5.gain(0, 0.6);
 }
+
 void loop() {
+  
   triggerBackingTrack();
   triggerXbeeActions();
   triggerButtonActions();
   triggerKeyboardActions();
+  
+  // turns led off after the set interval
+  for (int i = 0; i < buttonCount; i++) {
+  if (millis() - ledStartMillis[i] > blinkInterval) {
+    digitalWrite(ledPin[i], LOW);
+    }
+  }
 }
 
 
@@ -188,45 +211,46 @@ void triggerBackingTrack() {
 
 }
 
-// listening for commands from other mushrooms 
+// listening for commands from other mushrooms
 void triggerXbeeActions() {
 
   if (Serial1.available() > 0) {
     char inChar = (char)Serial1.read();
+    // quick indication that button pressed from other mushroom
+    for (int i = 0; i < buttonCount; i++) {
+    digitalWrite(ledPin[i],HIGH);
+    }
     playSound(inChar);
   }
 
   return;
 }
 
-
 // mushroom buttons pressed -> play file -> send serial command via Xbee
 void triggerButtonActions() {
-  // update debouncers
-  for (int i = 0; i < buttonCount; i++ ) {
+
+  for (int i = 0; i < buttonCount; i++) {
+    // update debouncers
     button[i].update();
-  }
-
-  // update debouncers
-  for (int i = 0; i < buttonCount; i++ ) {
-    if (button[i].fallingEdge()) {
+    if (button[i].risingEdge()) { // using rising edge because buttons are wired at NC
       // DO THE BUTTON PRESS ACTION
+      // PLAY CLIP
       sendAndPlay(i);
+      // TURN LED ON
+      turnOnLeds(i);
     }
-
   }
-
   return;
 }
 
-
-// using serial monitor (keypresses) to simulate mushrooms 
+// using serial monitor (keypresses) to simulate mushrooms
 void triggerKeyboardActions() {
 
   if (Serial.available() > 0) {
     char inChar = (char)Serial.read();
-    log("keyboard command: " + inChar);
-    sendAndPlay(getSound(inChar));  
+    log("keyboard command: ");
+    log(inChar);
+    sendAndPlay(getSound(inChar));
   }
 
   return;
@@ -246,30 +270,28 @@ void sendAndPlay(int buttonNumber) {
 // sending serial command to other Xbees + play file
 void sendAndPlay(char command) {
 
-  Serial1.print(command);
-  Serial1.flush();
-
-  log("sending :" + command);
+ 
+  log("sending :" + String(command));
+  //log(String(command));
 
   // react
   playSound(command);
 
+  //qazdelay(1000);
+  Serial1.write(command);
+  //Serial1.flush();
+
   return;
-  
 }
 
 // plays sound files, if given valid serial commands
 // otherwise, generates lots of logging and erros conditions
 void playSound(char command) {
 
-  log("Got serial command");
+  log("Got serial command :" + String(command));
   char* soundFileName = parseSoundCommand(command);
-  AudioPlaySdWav *mixerName = parseMixerCommand(command);
   log("Got file : " + String(soundFileName));
-  //playfile(soundFileName);
-
-  playfile(soundFileName, mixerName);
-
+  playfile(soundFileName);
   return;
 
 }
@@ -282,7 +304,8 @@ int getSound(char command) {
     }
   }
 
-  log("bad key:" + command);
+  log("bad key:");
+  log(command);
   return -1;
 }
 
@@ -294,92 +317,50 @@ char* parseSoundCommand(char command) {
   if (sound == -1) {
     //if error
     return soundFileName[errorSound];
-    
+
   }
 
   return soundFileName[sound];
 }
 
-AudioPlaySdWav *parseMixerCommand(char command) {
-  log("parseMixerCommand");
-  int sound = getSound(command);
-  if (sound == -1) {
-    //if error
-    return &sdWavArray[errorSound];
-    
-  }
-
-  return &sdWavArray[sound];
-}
-    
-
-void log (String message) {
-
-  if (DEBUG) {
-    Serial.println(getMushroom() + " " + message);
-  }
-
-  return;
-}
-
-/*
-// plays given filename
 void playfile(char * filename) {
 
   // i did it!
-  log("-device:" + String(getMushroom()) + " playing: " + String(filename));
-  //playSdWav1.play(filename);
-  
-  /*
-  for (i = 0; i< 12; i++) {
-  if (sdWavArray[i].isPlaying() ==false) {
-    Serial.println("Start playing " + i);
-    sdWavArray[i].play(filename); 
+  //log("-device:"); 
+  //log(String(getMushroom()));
+  log("playing: " + String(filename));
+  log(" ");
+  if (!playSdWav2.isPlaying()){
+  playSdWav2.play(filename);
+  } else {
+    playSdWav3.play(filename);
   }
-  
-
   return;
 
 }
-*/
-
-
-void playfile(char * filename, AudioPlaySdWav * mixerName) {
-
-  // i did it!
-  log("-device:" + String(getMushroom()) + " playing: " + String(filename));
-  //playSdWav1.play(filename);
-  //log(String(mixerName));
-  mixerName->play(filename);
-  /*
-  for (i = 0; i< 12; i++) {
-  if (sdWavArray[i].isPlaying() ==false) {
-    Serial.println("Start playing " + i);
-    sdWavArray[i].play(filename); 
-  }
-  */
-
-  return;
-
-}
-
-
-
 
 char getSoundCommand(int buttonId) {
-  int sound = buttonId + getMushroom() * buttonCount;
+  int sound = buttonId + getMushroom() * buttonCount; //buttonCount is a constant 3
   return soundCommand[sound];
 }
 
 
 int getMushroom() {
+  /*
   for (int i = 0; i < mushroomCount; i++ ) {
     if (LOW == digitalRead(mushroomSelectorPin[i]))
       return i;
-  }
+  }*/
+  boolean D_ON = false == digitalRead(mushroomSelectorPin[0]); // HIGH LOW is switched around on board ie. LOW is ON
+  boolean C_ON = false == digitalRead(mushroomSelectorPin[1]);
 
-  log("No Mushroom Selected!");
-  return -1;
+  return D_ON | C_ON << 1 ;
+
+  // mushroom 0 is when all off !C_ON && !D_ON 
+  // mushroom 1 is when only D_ON 
+  // mushroom 2 is when only C_ON 
+  // mushroom 3 is when D_ON && C_ON  
+  
 }
 
 void setupMushroomSelection() {
@@ -390,15 +371,39 @@ void setupMushroomSelection() {
 }
 
 void setupButtons() {
-  
   for (int i = 0; i < buttonCount; i++ ) {
     pinMode(buttonPin[i], INPUT_PULLUP);
-    button[i] = Bounce(buttonPin[i], buttonDebounceMs); 
+    button[i] = Bounce(buttonPin[i], buttonDebounceMs);
   }
+  return;
+}
 
+void setupLeds() {
+  for (int i = 0; i < buttonCount; i++) {
+    pinMode(ledPin[i], OUTPUT);
+    digitalWrite(ledPin[i], LOW);
+  }
+  return;
+}
+
+void turnOnLeds(int buttonId){
+  ledStartMillis[buttonId] = millis();
+  ledState = HIGH;
+  digitalWrite(ledPin[buttonId], ledState);
+  log("button " + String(buttonId));
+  log(" ");
   return;
 }
 
 
+void log (String message) {
 
+  if (DEBUG) {
+    Serial.print(getMushroom());
+    Serial.print(" ");
+    Serial.println(message);
+  }
+
+  return;
+}
 
